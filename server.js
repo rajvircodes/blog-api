b682ac32f1cc8b26d7394c0c635221c7e6b8a5ce
rajvircodes <rajvirsinhgohil004@gmail.com>
2026-08-14 04:32:35 +0530

    feat:User and Post model created, database connected

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
 require("dotenv").config();
 
 const app = require("./src/app");
+const connectDB = require("./src/config/db");
 const PORT = process.env.PORT;
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port:${PORT}`);
-});
+const startServer = async () => {
+  await connectDB();
+
+  app.listen(PORT, () => {
+    console.log(`Server is running on port:${PORT}`);
+  });
+};
+
+startServer();
--- src/app.js
+++ src/app.js
@@ -1,5 +1,11 @@
 const express = require("express");
 const app = express();
+const userRoutes = require("./routes/user.routes");
+
+const morgan = require("morgan");
+
+app.use(express.json());
+app.use(morgan("dev"));
 
 app.get("/", (req, res) => {
   res.json({
@@ -7,4 +13,6 @@ app.get("/", (req, res) => {
     message: "Hello from backend",
   });
 });
+
+app.use("/", userRoutes);
 module.exports = app;
