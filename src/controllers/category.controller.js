7b72cdb30d68aa3994f3a5f4554039a8d997079d
rajvircodes <rajvirsinhgohil004@gmail.com>
2026-08-24 06:41:50 +0530

    feat:validator, category model prepared

---
+++ src/controllers/category.controller.js
@@ -0,0 +1,12 @@
+const Category = require("../models/category.model");
+const slugify = require("slugify");
+
+const createCategory = async (req, res) => {};
+
+const getCategories = async (req, res) => {};
+
+const getCategoryById = async (req, res) => {};
+
+const updateCategory = async (req, res) => {};
+
+const deleteCategory = async (req, res) => {};
---
+++ src/models/category.model.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+const slugify = require("slugify");
+
+const categorySchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Category name is required"],
+      unique: true,
+      trim: true,
+    },
+    slug: {
+      type: String,
+      unique: true,
+      lowercase: true,
+    },
+  },
+  { timestamps: true },
+);
+
+categorySchema.pre("save", function (next) {
+  if (this.isModified("name")) {
+    this.slug = slugify(this.name, { lower: true, strict: true });
+  }
+  next();
+});
+
+module.exports = mongoose.model("Category", categorySchema);
