55fd5a09748d9fcf9bf29a8491d47836db8ff4dd
rajvircodes <rajvirsinhgohil004@gmail.com>
2026-09-07 08:32:33 +0530

    refactor: remove unused hello route and express-validator imports

--- src/app.js
+++ src/app.js
@@ -1,5 +1,4 @@
 const express = require("express");
-const { query, validationResult, matchedData } = require("express-validator");
 const app = express();
 const userRoutes = require("./routes/user.routes");
 const postRoutes = require("./routes/post.routes");
@@ -21,15 +20,6 @@ app.get("/", (req, res) => {
     success: true,
     message: "Hello from backend",
   });
-});
-
-app.get("/hello", query("person").notEmpty().escape(), (req, res) => {
-  const result = validationResult(req);
-  if (result.isEmpty()) {
-    const data = matchedData(req);
-    return res.send(`Hello ${data.person}!`);
-  }
-  res.send({ errors: result.array() });
 });
 
 app.use("/api/v1/auth", userRoutes);
